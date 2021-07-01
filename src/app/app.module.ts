20c7f85c26aa50606167057bb0bf53cde8e9ec0a
Karen Christina <karen.nascimento.developer@gmail.com>
2021-06-30 21:02:35 -0300

    componentes description and input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { DescriptionComponent } from './description/description.component';
+import { InputUrlComponent } from './description/input-url/input-url.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    DescriptionComponent,
+    InputUrlComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/description/input-url/input-url.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input-url',
+  templateUrl: './input-url.component.html',
+  styleUrls: ['./input-url.component.scss']
+})
+export class InputUrlComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
